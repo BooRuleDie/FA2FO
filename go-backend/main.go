0f0ff6ec61c8b394d0cbb2516926ee2b0a922b96
BooRuleDie <booruledie@gmail.com>
2025-03-03 13:55:13 +0300

    Handler interface & HTTP Serve covered

--- go-backend/main.go
+++ go-backend/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+const appPort = ":8080"
+
+type application struct {
+	addr string
+}
+
+func (s *application) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		switch r.URL.Path {
+		case "/":
+			rw.Write([]byte("index page"))
+			return
+		case "/users":
+			rw.Write([]byte("users endpoint"))
+			return
+		default:
+			rw.Write([]byte("unknown path"))
+			return
+		}
+	default:
+		rw.Write([]byte("method not allowed"))
+		return
+	}
+
+}
 
 func main() {
-	fmt.Println("I'm running 3...")
+	app := &application{addr: appPort}
+	srv := &http.Server{
+		Addr: app.addr,
+		Handler: app,
+	}
+	
+	log.Println("server started listening!")
+	log.Fatal(srv.ListenAndServe())
 }
