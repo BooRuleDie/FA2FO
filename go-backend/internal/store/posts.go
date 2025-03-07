350fcfb1788d8c9f2b33bdb8fda02442a0eebee3
BooRuleDie <booruledie@gmail.com>
2025-03-07 17:31:51 +0300

    repository patterns implemented for postgreSQL

--- go-backend/internal/store/posts.go
+++ go-backend/internal/store/posts.go
@@ -3,10 +3,25 @@ package store
 import (
 	"context"
 	"database/sql"
+	"time"
+
+	"github.com/lib/pq"
 )
 
+// Post Model, you can put it into a seperate
+// package as well
+type Post struct {
+	ID        int64         `json:"id"`
+	Title     string        `json:"title"`
+	Content   string        `json:"content"`
+	UserID    int64         `json:"user_id"`
+	Tags      []string      `json:"tags"`
+	CreatedAt time.Duration `json:"created_at"`
+	UpdatedAt time.Duration `json:"updated_at"`
+}
+
 type postsRepository interface {
-	Create(context.Context) error
+	Create(context.Context, *Post) error
 }
 
 // postgreSQL Posts struct that'll satisfy
@@ -21,7 +36,24 @@ func newPostsRepo(db *sql.DB) *pqPosts {
 	}
 }
 
-func (ps *pqPosts) Create(ctx context.Context) error {
+func (ps *pqPosts) Create(ctx context.Context, post *Post) error {
+	query := `
+		INSERT INTO posts(content, title, user_id, tags)
+		VALUES ($1, $2, $3, $4) RETURNING id, created_at, updated_at
+	`
 
-	return nil
+	err := ps.db.QueryRowContext(
+		ctx,
+		query,
+		post.Content,
+		post.Title,
+		post.UserID,
+		pq.Array(post.Tags),
+	).Scan(
+		&post.ID,
+		&post.CreatedAt,
+		&post.UpdatedAt,
+	)
+
+	return err
 }
--- go-backend/internal/store/users.go
+++ go-backend/internal/store/users.go
@@ -3,10 +3,23 @@ package store
 import (
 	"context"
 	"database/sql"
+	"time"
 )
 
+// User Model, could be in a
+// seperate package
+type User struct {
+	ID        int64          `json:"id"`
+	Username  string         `json:"username"`
+	Email     string         `json:"email"`
+	// password field is excluded from JSON marshaling with `json:"-"`
+	Password  string         `json:"-"` 
+	CreatedAt time.Duration  `json:"created_at"`
+	UpdatedAt time.Duration  `json:"updated_at"`
+}
+
 type usersRepository interface {
-	Create(context.Context) error
+	Create(context.Context, *User) error
 }
 
 // postgreSQL Users struct that'll satisfy
@@ -21,7 +34,25 @@ func newUsersRepo(db *sql.DB) *pqUsers {
 	}
 }
 
-func (ps *pqUsers) Create(ctx context.Context) error {
+func (us *pqUsers) Create(ctx context.Context, user *User) error {
+	query := `
+		INSERT INTO Users(username, password, email)
+		VALUES($1, $2, $3) RETURNING id, created_at, updated_at
+	`
 	
-	return nil
+	err := us.db.QueryRowContext(
+		ctx, 
+		query, 
+		user.Username, 
+		// should be hashed before inserting
+		// or let database handle the hashing
+		user.Password, 
+		user.Email,
+	).Scan(
+		&user.ID, 
+		&user.CreatedAt, 
+		&user.UpdatedAt,
+	)
+	
+	return err
 }
