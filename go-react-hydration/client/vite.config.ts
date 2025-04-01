fe995d7d22fbe0c7c7107d6b361e8c194aecea4d
BooRuleDie <booruledie@gmail.com>
2025-04-01 11:03:38 +0300

    server & client checkpoint

---
+++ go-react-hydration/client/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  build: {
+    outDir: 'dist/assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
+      }
+    }
+  }
+})
